bb8841bd2e1313ec5e8d32c4986e6a9f63432015
benjamin menkuec <benjamin@menkuec.de>
2015-08-23 22:31:53 +0200

    updates dirs, they include UCSC tools now

--- scripts/init_path.bat
+++ scripts/init_path.bat
@@ -1,1 +1,1 @@
-set PATH=%PATH%;P:\git\nexus-tools2_build\bin\release;P:\git\seqan_build\debug-vs15\bin\Release;P:\cygwin64\bin;P:\cygwin64\home\samtools-1.2
+set PATH=%PATH%;P:\git\nexus-tools2_build\bin\release;P:\git\seqan_build\debug-vs15\bin\Release;P:\cygwin64\bin;P:\cygwin64\home\samtools-1.2;P:\cygwin64\home\ucsc\bin
