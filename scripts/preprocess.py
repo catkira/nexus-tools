346eebf36e6f436d60f3bdcbc31e01b20e4f019d
benjamin menkuec <benjamin@menkuec.de>
2015-08-26 15:52:00 +0200

    [FIX] fix for linux
    [FEATURE] --exo parameter

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -4,6 +4,7 @@ import sys
 import os.path
 import subprocess
 import argparse
+import platform
 
 #print 'Number of arguments:', len(sys.argv), 'arguments.'
 #print 'Argument List:', str(sys.argv)
@@ -15,11 +16,12 @@ flexbar_ml = "18";
 flexbar_tnum = "4";
 flexbarAdapterFilename = "../data/adapters.fa";
 flexbarBarcodeFilename = "../data/barcodes.fa";
-bowtieLocation = ""
 dataDir = os.path.abspath("../data/") + "/"
 
 parser = argparse.ArgumentParser(description="Preprocess fastq files and do mapping")
 parser.add_argument('--output', type=str)
+parser.add_argument('--exo', action='store_true')
+parser.add_argument('--bowtie_location', nargs='?')
 parser.add_argument('input_file')
 parser.add_argument('--data_dir', type=str)
 parser.add_argument('genome', type=str);
@@ -33,7 +35,12 @@ print "Genome: " + results.genome
 if results.data_dir is not None:
  dataDir = os.path.abspath(results.data_dir) + "/"
 
-genomeFilename = results.genome;
+genomeFilename = results.genome
+bowtieLocation = os.path.abspath(results.bowtie_location) + "/"
+
+if(platform.system() == "Windows" and bowtieLocation == None):
+ print "Bowtie location is required under windows"
+ sys.exit()
 
 if results.output is not None:
  outFilenamePrefix, file_extension = os.path.splitext(results.output)
@@ -52,8 +59,13 @@ inFilenamePrefixWithoutPath, temp = os.path.splitext(inFilenamePrefixWithoutPath
 outputDir = dataDir + inFilenamePrefixWithoutPath
 flexbarOutputFilename = outputDir + "/" + inFilenamePrefixWithoutPath + inFileExtension
 
-args = ("seqan_flexbar", results.input_file, "-tl", "5", "-tt", "-t", "-ss", "-tnum", flexbar_tnum,"-er",flexbar_er, "-ol", flexbar_ol, "-fm", flexbar_fm, "-ml", flexbar_ml, "-b", flexbarBarcodeFilename, "-a", flexbarAdapterFilename,"-o", flexbarOutputFilename)
-#args = ("seqan_flexbar", results.input_file, "-tl", "5","-b", flexbarBarcodeFilename, "-a", flexbarAdapterFilename,"-o", flexbarOutputFilename)
+if results.exo:
+ print "processing Chip-Exo data"
+ args = ("seqan_flexbar", results.input_file, "-tt", "-t", "-ss", "-tnum", flexbar_tnum,"-er",flexbar_er, "-ol", flexbar_ol, "-fm", flexbar_fm, "-ml", flexbar_ml, "-a", flexbarAdapterFilename,"-o", flexbarOutputFilename)
+else:
+ print "processing Chip-Nexus data"
+ args = ("seqan_flexbar", results.input_file, "-tl", "5", "-tt", "-t", "-ss", "-tnum", flexbar_tnum,"-er",flexbar_er, "-ol", flexbar_ol, "-fm", flexbar_fm, "-ml", flexbar_ml, "-b", flexbarBarcodeFilename, "-a", flexbarAdapterFilename,"-o", flexbarOutputFilename)
+ #args = ("seqan_flexbar", results.input_file, "-tl", "5","-b", flexbarBarcodeFilename, "-a", flexbarAdapterFilename,"-o", flexbarOutputFilename)
 if not os.path.exists(outputDir):
  os.makedirs(outputDir)
 print args
@@ -69,7 +81,10 @@ genomeIndex, file_extension = os.path.splitext(tail)
  
 # check if bowtie index already exists
 if (os.path.isfile(dataDir + genomeIndex + ".1.ebwt") == False):
- args = ("python",  bowtieLocation + "bowtie-build", "-o", "1", genomeFilename, dataDir+genomeIndex)
+ if(platform.system() == "Linux" or platform.system() == "Linux2"):
+  args = (bowtieLocation + "bowtie-build", "-o", "1", genomeFilename, dataDir+genomeIndex)
+ else:
+  args = ("python",  bowtieLocation + "bowtie-build", "-o", "1", genomeFilename, dataDir+genomeIndex)
  popen = subprocess.Popen(args, stdout=subprocess.PIPE)
  popen.wait()
  output = popen.stdout.read()
@@ -78,8 +93,10 @@ if (os.path.isfile(dataDir + genomeIndex + ".1.ebwt") == False):
   print "error"
   sys.exit() 
 
-
-bowtieInputFilename = outputDir + "/" + inFilenamePrefixWithoutPath + "_matched_barcode" + inFileExtension
+if results.exo:
+ bowtieInputFilename = outputDir + "/" + inFilenamePrefixWithoutPath + inFileExtension
+else:
+ bowtieInputFilename = outputDir + "/" + inFilenamePrefixWithoutPath + "_matched_barcode" + inFileExtension
 bowtieOutputFilename = outputDir + "/" + inFilenamePrefixWithoutPath + ".sam"
 
 args = ("python", bowtieLocation + "bowtie", "-S", "-p", "4", "--chunkmbs", "512", "-k", "1", "-m", "1", "-v", "2", "--strata", "--best", dataDir+genomeIndex, bowtieInputFilename, bowtieOutputFilename)
@@ -115,4 +132,4 @@ if popen.returncode != 0:
  
 #cleanup
 os.remove(bowtieOutputFilename)
-os.remove(outputDir + "/" + inFilenamePrefixWithoutPath + ".bam")
+os.remove(nexusOutputFilename)
