7b8808ba3ae1ad40bab2ac8518ed69f611c51f2d
benjamin menkuec <benjamin@menkuec.de>
2015-10-13 10:56:10 +0200

    removed ting from compilation

--- build_linux_release.sh
+++ build_linux_release.sh
@@ -3,15 +3,11 @@ mkdir nexus-tools_build
 cd nexus-tools_build
 export CXX=/usr/bin/g++-5
 cmake -DLINUX_STATIC:BOOL=ON -DCMAKE_BUILD_TYPE=Release ../nexus-tools
-make ting
 make flexbar++
 make nexus-pre
 make MappingAnalyzer
-cp -f bin/ting ../nexus-tools/bin/
 cp -f bin/flexbar++ ../nexus-tools/bin/
 cp -f bin/nexus-pre ../nexus-tools/bin/
 cp -f bin/MappingAnalyzer ../nexus-tools/bin/
 
-cp -f bin/flexbar++ ~/Q/bin
-cp -f bin/nexus-pre ~/Q/bin
 
