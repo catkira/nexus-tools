3dae5102c8cee7f1c558faeea76a56e075c58b8f
Benjamin Menkuec <benjamin@menkuec.de>
2015-10-20 13:21:31 +0200

    updated linux binaries

--- build_linux_release.sh
+++ build_linux_release.sh
@@ -3,10 +3,10 @@ mkdir nexus-tools_build
 cd nexus-tools_build
 export CXX=/usr/bin/g++-5
 cmake -DLINUX_STATIC:BOOL=ON -DCMAKE_BUILD_TYPE=Release ../nexus-tools
-make flexbar++
+make flexcat
 make nexus-pre
 make MappingAnalyzer
-cp -f bin/flexbar++ ../nexus-tools/bin/
+cp -f bin/flexcat ../nexus-tools/bin/
 cp -f bin/nexus-pre ../nexus-tools/bin/
 cp -f bin/MappingAnalyzer ../nexus-tools/bin/
 
