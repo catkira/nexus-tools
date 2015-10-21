ec75337be9674a62d8f6c0019a63e76b0b926ec9
benjamin menkuec <benjamin@menkuec.de>
2015-10-21 17:13:11 +0200

    code cleanup

---
+++ flexcat/produce_transform_consume.h
@@ -0,0 +1,290 @@
+// ==========================================================================
+// Author: Benjamin Menkuec <benjamin@menkuec.de>
+// ==========================================================================
+#pragma once
+
+#include <future>
+#include <functional>
+
+#include "semaphore.h"
+#include "helper_functions.h"
+
+// reads read sets from hd and puts them into slots, waits if no free slots are available
+template<typename TSource, typename TItem, bool useSemaphore>
+struct Produce
+{
+    using item_type = TItem;
+
+private:
+    TSource _source;
+    std::vector<std::atomic<TItem*>> _tlsItems;
+    std::thread _thread;
+    std::atomic_bool _eof;
+    unsigned int _sleepMS;
+    LightweightSemaphore slotEmptySemaphore;
+    LightweightSemaphore readAvailableSemaphore;
+
+public:
+    Produce(TSource& source, const unsigned int numThreads, unsigned int sleepMS)
+        : _source(source),  _tlsItems(numThreads + 1),
+        _eof(false), _sleepMS(sleepMS)
+    {
+        for (auto& item : _tlsItems)
+            item.store(nullptr);  // fill initialization does not work for atomics
+    }
+    ~Produce()
+    {
+        if (_thread.joinable())
+            _thread.join();
+    }
+    void start()
+    {
+        /*
+        - check if empty slot is available, if yes, read data into it
+        - set eof=true if no more data available or max_number_of_reads reached
+        - return from thread if eof == true
+        - go to sleep if no free slot is available
+        */
+        _thread = std::thread([this]()
+        {
+            std::unique_ptr <TItem> currentItem;
+            bool noEmptySlot = true;
+            while (true)
+            {
+                noEmptySlot = true;
+                for (auto& item : _tlsItems)
+                {
+                    if (item.load(std::memory_order_relaxed) == nullptr)
+                    {
+                        noEmptySlot = false;
+                        currentItem = std::make_unique<TItem>();
+                        if (_source.give(currentItem))
+                            item.store(currentItem.release(), std::memory_order_relaxed);
+                        else
+                            _eof.store(true, std::memory_order_release);
+
+                        if (useSemaphore && _eof.load(std::memory_order_relaxed))  // wakeup all potentially waiting threads so that they can be joined
+                            readAvailableSemaphore.signal(_tlsItems.size());
+                        else if (useSemaphore)
+                            readAvailableSemaphore.signal();
+                    }
+                    if (_eof.load(std::memory_order_relaxed))
+                        return;
+                }
+                if (noEmptySlot)  // no empty slow was found, wait a bit
+                {
+                    if (useSemaphore)
+                        slotEmptySemaphore.wait();
+                    else
+                        std::this_thread::sleep_for(std::chrono::milliseconds(_sleepMS));
+                }
+            }
+        });
+    }
+    inline bool eof() const noexcept
+    {
+        return _eof.load(std::memory_order_acquire);
+    }
+    bool idle() noexcept
+    {
+        if (!_eof.load(std::memory_order_acquire))
+            return false;
+        for (auto& item : _tlsItems)
+            if (item.load(std::memory_order_relaxed))
+                return false;
+        return true;
+    }
+    /*
+    do the following steps sequentially
+    - check if any slot contains data, if yes take data out and return true
+    - check if eof is reached, if yes return false
+    - go to sleep until data is available
+    */
+    bool getItem(std::unique_ptr<TItem>& returnItem) noexcept
+    {
+        TItem* temp = nullptr;
+        while (true)
+        {
+            bool eof = _eof.load(std::memory_order_acquire);
+            for (auto& item : _tlsItems)
+            {
+                if ((temp = item.load(std::memory_order_relaxed)) != nullptr)
+                {
+                    if (item.compare_exchange_strong(temp, nullptr, std::memory_order_relaxed))
+                    {
+                        returnItem.reset(temp);
+                        if (useSemaphore)
+                            slotEmptySemaphore.signal();
+                        return true;
+                    }
+                }
+            }
+            //std::cout << std::this_thread::get_id() << "-2" << std::endl;
+            if (eof) // only return if _eof == true AND all the slots are empty -> therefore read eof BEFORE checking the slots
+                return false;
+            else if (useSemaphore)
+                readAvailableSemaphore.wait();
+            else
+                std::this_thread::sleep_for(std::chrono::milliseconds(_sleepMS));
+        }
+        return false;
+    };
+};
+
+
+template <typename TProducer, typename TTransformer, typename TConsumer>
+struct PTC_unit
+{
+private:
+    // main thread variables
+    TProducer& _producer;
+    TTransformer& _transformer;
+    TConsumer& _consumer;
+    std::vector<std::thread> _threads;
+
+public:
+    PTC_unit(TProducer& producer, TTransformer& transformer, TConsumer& consumer, const unsigned int numThreads) :
+        _producer(producer), _transformer(transformer), _consumer(consumer), _threads(numThreads){};
+
+    void start()
+    {
+        _producer.start();
+        _consumer.start();
+        for (auto& _thread : _threads)
+        {
+            _thread = std::thread([this]()
+            {
+                std::unique_ptr<TProducer::item_type> item;
+                while (_producer.getItem(item))
+                {
+                    _consumer.pushItem(_transformer.transform(std::move(item)));
+                }
+            });
+        }
+    }
+    void waitForFinish()
+    {
+        for (auto& _thread : _threads)
+            if (_thread.joinable())
+                _thread.join();
+        while (!_consumer.idle())
+        {
+        };
+        _consumer.shutDown();
+    }
+    bool finished() noexcept
+    {
+        return _producer.eof();
+    }
+
+};
+
+
+
+
+template<typename TSink, typename TItem, typename TProgramParams, bool useSemaphore>
+struct Reduce
+{
+public:
+    using item_type = TItem;
+private:
+    std::vector<std::atomic<TItem*>> _tlsItems;
+    std::thread _thread;
+    std::atomic_bool _run;
+    unsigned int _sleepMS;
+    LightweightSemaphore itemAvailableSemaphore;
+    LightweightSemaphore slotEmptySemaphore;
+    TSink& _sink;
+
+public:
+    Reduce(TSink& sink, const unsigned int numThreads, unsigned int sleepMS)
+        : _sink(sink), _tlsItems(numThreads + 1),
+        _run(false), _sleepMS(sleepMS)
+    {
+        for (auto& item : _tlsItems)
+            item.store(nullptr);  // fill initialization does not work for atomics
+    }
+    ~Reduce()
+    {
+        _run = false;
+        if (_thread.joinable())
+            _thread.join();
+    }
+    void start()
+    {
+        _run = true;
+        _thread = std::thread([this]()
+        {
+            std::unique_ptr<TItem> currentItem;
+            bool nothingToDo = false;
+            while (_run.load(std::memory_order_relaxed) || !nothingToDo)
+            {
+                nothingToDo = true;
+                for (auto& item : _tlsItems)
+                {
+                    if (item.load(std::memory_order_relaxed) != nullptr)
+                    {
+                        currentItem.reset(item.load(std::memory_order_relaxed));
+                        item.store(nullptr, std::memory_order_release); // make the slot free again
+                        if (useSemaphore)
+                            slotEmptySemaphore.signal();
+                        nothingToDo = false;
+
+                        //std::this_thread::sleep_for(std::chrono::milliseconds(1));  // used for debuggin slow hd case
+                        _sink.accept(std::move(*currentItem));
+                    }
+                }
+                if (nothingToDo)
+                {
+                    //std::cout << std::this_thread::get_id() << "-4" << std::endl;
+                    if (useSemaphore)
+                    {
+                        itemAvailableSemaphore.wait();
+                    }
+                    else
+                        std::this_thread::sleep_for(std::chrono::milliseconds(_sleepMS));
+                }
+            }
+        });
+    }
+    void pushItem(std::unique_ptr<TItem> newItem)     // blocks until item could be added
+    {
+        while (true)
+        {
+            for (auto& item : _tlsItems)
+            {
+                if (item.load(std::memory_order_relaxed) == nullptr)
+                {
+                    TItem* temp = nullptr;
+                    if (item.compare_exchange_strong(temp, newItem.get(), std::memory_order_acq_rel))  // acq_rel to make sure, that idle does not return true before all reads are written
+                    {
+                        newItem.release();
+                        if (useSemaphore)
+                            itemAvailableSemaphore.signal();
+                        return;
+                    }
+                }
+            }
+            //std::cout << std::this_thread::get_id() << "-3" << std::endl;
+            if (useSemaphore)
+                slotEmptySemaphore.wait();
+            else
+                std::this_thread::sleep_for(std::chrono::milliseconds(_sleepMS));
+        }
+    }
+    void shutDown()
+    {
+        _run.store(false, std::memory_order_relaxed);
+        if (useSemaphore)
+            itemAvailableSemaphore.signal();
+        if (_thread.joinable())
+            _thread.join();
+    }
+    bool idle() noexcept
+    {
+        for (auto& item : _tlsItems)
+            if (item.load(std::memory_order_acquire) != nullptr) // acq to make sure, that idle does not return true before all reads are written
+                return false;
+        return true;
+    }
+};
--- flexcat/read_reader.h
+++ flexcat/read_reader.h
@@ -10,136 +10,34 @@
 
 #include "semaphore.h"
 
-// reads read sets from hd and puts them into slots, waits if no free slots are available
-template<template<typename> class TRead, typename TSeq, typename TProgramParams, typename TInputFileStreams, bool useSemaphore>
+template<template<typename> class TRead, typename TSeq, typename TProgramParams, typename TInputFileStreams>
 struct ReadReader
 {
 public:
-    using TReadSet = std::vector<TRead<TSeq>>;
+    using itemType = std::vector<TRead<TSeq>>;
 private:
-    const TProgramParams& _programParams;
     TInputFileStreams& _inputFileStreams;
-    std::vector<std::atomic<TReadSet*>> _tlsReadSets;
-    std::thread _thread;
-    std::atomic_bool _eof;
-    unsigned int _sleepMS;
+    const TProgramParams& _programParams;
     unsigned int _numReads;
-    LightweightSemaphore slotEmptySemaphore;
-    LightweightSemaphore readAvailableSemaphore;
 
 public:
-    ReadReader(const TProgramParams& programParams, TInputFileStreams& inputFileStreams, unsigned int sleepMS)
-        :_programParams(programParams), _inputFileStreams(inputFileStreams), _tlsReadSets(_programParams.num_threads + 1),
-        _eof(false), _sleepMS(sleepMS), _numReads(0)
+    ReadReader(TInputFileStreams& inputFileStreams, const TProgramParams& programParams)
+        : _inputFileStreams(inputFileStreams), _programParams(programParams), _numReads(0) {};
+    bool give(std::unique_ptr<std::vector<TRead<TSeq>>>& item)
     {
-        for (auto& readSet : _tlsReadSets)
-            readSet.store(nullptr);  // fill initialization does not work for atomics
-    }
-    ~ReadReader()
-    {
-        if (_thread.joinable())
-            _thread.join();
-    }
-    void start()
-    {
-        /*
-        - check if empty slot is available, if yes, read data into it
-            - set eof=true if no more data available or max_number_of_reads reached
-        - return from thread if eof == true
-        - go to sleep if no free slot is available
-        */
-        _thread = std::thread([this]()
-        {
-            std::unique_ptr <TReadSet> currentReadSet;
-            bool noEmptySlot = true;
-            while (true)
-            {
-                noEmptySlot = true;
-                for (auto& readSet : _tlsReadSets) 
-                {
-                    if (readSet.load(std::memory_order_relaxed) == nullptr)
-                    {
-                        noEmptySlot = false;
-                        currentReadSet = std::make_unique<TReadSet>(_programParams.records);
-                        try {
-                            readReads(*currentReadSet, _programParams.records, _inputFileStreams);
-                        }
-                        catch (std::exception& e){
-                            std::cout << "exception while reading :" << e.what() << " after read " << _numReads << std::endl;
-                            throw(e);
-                        }
-                        loadMultiplex(*currentReadSet, _programParams.records, _inputFileStreams.fileStreamMultiplex);
-                        _numReads += currentReadSet->size();
-                        const bool empty = currentReadSet->empty();
-                        readSet.store(currentReadSet.release(), std::memory_order_relaxed);
-                        if (empty || _numReads >= _programParams.firstReads)    // no more reads available or maximum read number reached -> dont do further reads
-                            _eof.store(true, std::memory_order_release);
-
-                        if (useSemaphore && _eof.load(std::memory_order_relaxed))  // wakeup all potentially waiting threads so that they can be joined
-                            readAvailableSemaphore.signal(_programParams.num_threads);
-                        else if (useSemaphore)
-                            readAvailableSemaphore.signal();
-                    }
-                    if (_eof.load(std::memory_order_relaxed))
-                        return;
-                }
-                if (noEmptySlot)  // no empty slow was found, wait a bit
-                {
-                    if (useSemaphore)
-                        slotEmptySemaphore.wait();
-                    else
-                        std::this_thread::sleep_for(std::chrono::milliseconds(_sleepMS));
-                }
-            }
-        });
-    }
-    inline bool eof() const noexcept
-    {
-        return _eof.load(std::memory_order_acquire);
-    }
-    bool idle() noexcept
-    {
-        if (!_eof.load(std::memory_order_acquire))
+        try {
+            readReads(*item, _programParams.records, _inputFileStreams);
+        }
+        catch (std::exception& e) {
+            std::cout << "exception while reading :" << e.what() << " after read " << _numReads << std::endl;
+            throw(e);
+        }
+        loadMultiplex(*item, _programParams.records, _inputFileStreams.fileStreamMultiplex);
+        _numReads += item->size();
+        if (item->empty() || _numReads >= _programParams.firstReads)    // no more reads available or maximum read number reached -> dont do further reads
             return false;
-        for (auto& readSet : _tlsReadSets)
-            if (readSet.load(std::memory_order_relaxed))
-                return false;
         return true;
     }
-    /*
-    do the following steps sequentially
-    - check if any slot contains data, if yes take data out and return true
-    - check if eof is reached, if yes return false
-    - go to sleep until data is available
-    */
-    bool getReads(std::unique_ptr<TReadSet>& reads) noexcept
-    {
-        TReadSet* temp = nullptr;
-        while (true)
-        {
-            bool eof = _eof.load(std::memory_order_acquire);
-            for (auto& readSet : _tlsReadSets)
-            {
-                if ((temp = readSet.load(std::memory_order_relaxed)) != nullptr)
-                {
-                    if (readSet.compare_exchange_strong(temp, nullptr, std::memory_order_relaxed))
-                    {
-                        reads.reset(temp);
-                        if (useSemaphore)
-                            slotEmptySemaphore.signal();
-                        return true;
-                    }
-                }
-            }
-            //std::cout << std::this_thread::get_id() << "-2" << std::endl;
-            if (eof) // only return if _eof == true AND all the slots are empty -> therefore read eof BEFORE checking the slots
-                return false;
-            else if (useSemaphore)
-                readAvailableSemaphore.wait();
-            else
-                std::this_thread::sleep_for(std::chrono::milliseconds(_sleepMS));
-        }
-        return false;
-    };
 };
+
 
