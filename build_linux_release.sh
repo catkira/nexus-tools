413fe00a98af61178cdd55497e97dcd6ffd332f2
Benjamin Menkuec <benjamin@menkuec.de>
2015-11-09 14:37:02 +0100

    updated static linux binaries

--- build_linux_release.sh
+++ build_linux_release.sh
@@ -5,11 +5,11 @@ cd nexus-tools_build
 export CXX=/usr/bin/g++-5
 cmake -DLINUX_STATIC:BOOL=OFF -DCMAKE_BUILD_TYPE=Release ../nexus-tools
 make flexcat
-make nexus-pre
+make nexcat
 make MappingAnalyzer
 make 5PrimeEndCounter
 cp -f bin/flexcat ../nexus-tools/bin/
-cp -f bin/nexus-pre ../nexus-tools/bin/
+cp -f bin/nexcat ../nexus-tools/bin/
 cp -f bin/MappingAnalyzer ../nexus-tools/bin/
 cp -f bin/5PrimeEndCounter ../nexus-tools/bin/
 
