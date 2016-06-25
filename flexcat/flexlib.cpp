988b17b37ccfe92e8a53600db9ceb564a0a44e8a
Benjamin Menkuec <benjamin@menkuec.de>
2016-06-25 10:28:46 +0200

    do quality trimming before adapter trimming

--- flexcat/flexlib.cpp
+++ flexcat/flexlib.cpp
@@ -456,8 +456,8 @@ int mainLoop(TRead<TSeq>, const ProgramParams& programParams, InputFileStreams& 
         preprocessingStage(processingParams, *reads, stats);
         if (demultiplexingStage(demultiplexingParams, *reads, esaFinder, stats) != 0)
             std::cerr << "DemultiplexingStage error" << std::endl;
-        adapterTrimmingStage(*reads, tlsBlock);
         qualityTrimmingStage(qualityTrimmingParams, *reads, stats);
+        adapterTrimmingStage(*reads, tlsBlock);
         postprocessingStage(processingParams, *reads, stats);
         stats.processTime = std::chrono::duration_cast<std::chrono::duration<float>>(std::chrono::steady_clock::now() - t1).count();
         return std::make_unique<std::tuple<decltype(reads), decltype(demultiplexingParams.barcodeIds), TStats>>(std::make_tuple(std::move(reads), demultiplexingParams.barcodeIds, stats));
